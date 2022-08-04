c45c2a7f7bc005ae2bbdde8f8ac5188d78a4e600
Kojji <fernandokojiyama@gmail.com>
2022-08-03 21:01:19 -0300

    fix: folder path cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://localhost:8080',
+    supportFile: "test/cypress/support/e2e.js",
+    specPattern: "test/cypress/e2e/spec.cy.js"
   },
+  fixturesFolder: "test/cypress/fixtures",
+  videosFolder: "test/cypress/videos",
+  downloadsFolder: "test/cypress/downloads",
+  screenshotsFolder: "test/cypress/screenshots",
 });
